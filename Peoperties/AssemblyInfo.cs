673e18e85036f628d1a6e2c4f5db8e0fa1d42839
n0xa <214826087+rk0exn@users.noreply.github.com>
2025-10-18 12:22:32 +0900

    Update to v1.3

--- Peoperties/AssemblyInfo.cs
+++ Peoperties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) 2025 rk0exn All rights reserved.
-// DWM_Run v1.2
+// DWM_Run v1.3
 
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0ff692b5-ef71-45c9-9c84-5633b0c89a6f")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
